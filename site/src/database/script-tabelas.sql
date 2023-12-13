fcd19ec78e2a1c8d4bdf5dc92a2114692984e3e8
LucasCMendes10 <lucas.mendes@sptech.school>
2023-12-13 11:10:06 -0300

    script de create table

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -1,62 +1,42 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
-/*
-comandos para mysql - banco local - ambiente de desenvolvimento
-*/
-
 CREATE DATABASE bdDesafioPI;
 
-USE aquatech;
+USE bdDesafioPI;
 
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14)
+create table empresa (
+idEmpresa int primary key auto_increment,
+nome varchar(50),
+cnpj char(14),
+email varchar(60),
+logradouro varchar(70),
+numero int
 );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+create table usuario (
+idUsuario int auto_increment,
+nome varchar(45),
+email varchar(50),
+senha varchar(20),
+cpf char(11),
+fkEmpresa int,
+primary key (idUsuario, fkEmpresa),
+constraint fkempresausuario foreign key (fkEmpresa) references empresa(idEmpresa)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+create table sensorTcrt (
+idSensor int auto_increment,
+nomeSensor varchar(45),
+fkEmpresa int,
+primary key (idSensor, fkEmpresa),
+constraint fkempresasensor foreign key (fkEmpresa) references empresa(idEmpresa)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+create table registro (
+idRegistro int,
+leitura int,
+fkSensor int,
+primary key (idRegistro, fkSensor),
+constraint fksensorregistro foreign key (fkSensor) references sensorTcrt(idSensor)
 );
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, cnpj) values ('Empresa 1', '00000000000000');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
 
 create user "desafioLocal"@'localhost' identified by "desafioPI";
 grant all privileges on bdDesafioPI.* to "desafioLocal"@'localhost';
